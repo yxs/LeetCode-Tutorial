c48dd95b485a9f3fcbc0460b2fa1383de36c5561
yxs <cedarxsye@outlook.com>
2020-08-02 21:52:13 +0800

    One question per day, August 1

---
+++ problems/632.smallest-range-covering-elements-from-k-lists/Solution.py
@@ -0,0 +1,37 @@
+import collections
+
+# ?
+class Solution:
+    def smallestRange(self, nums: List[List[int]]) -> List[int]:
+        n = len(nums)
+        indices = collections.defaultdict(list)
+        xMin, xMax = 10 ** 9, -(10 ** 9)
+        for i, vec in enumerate(nums):
+            for x in vec:
+                indices[x].append(i)
+            xMin = min(xMin, *vec)
+            xMax = max(xMax, *vec)
+
+        freq = [0] * n
+        inside = 0
+        left, right = xMin, xMin - 1
+        bestLeft, bestRight = xMin, xMax
+
+        while right < xMax:
+            right += 1
+            if right in indices:
+                for x in indices[right]:
+                    freq[x] += 1
+                    if freq[x] == 1:
+                        inside += 1
+                while inside == n:
+                    if right - left < bestRight - bestLeft:
+                        bestLeft, bestRight = left, right
+                    if left in indices:
+                        for x in indices[left]:
+                            freq[x] -= 1
+                            if freq[x] == 0:
+                                inside -= 1
+                    left += 1
+
+        return [bestLeft, bestRight]
