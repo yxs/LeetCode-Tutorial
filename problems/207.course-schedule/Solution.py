814e6901b27ee16530811310da3995fec4e96cb4
yxs <cedarxsye@outlook.com>
2020-08-04 22:18:50 +0800

    One question per day, August 4

---
+++ problems/207.course-schedule/Solution.py
@@ -0,0 +1,56 @@
+import collections
+from collections import deque
+
+
+class SolutionDFS:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        edges = collections.defaultdict(list)
+        visited = [0] * numCourses
+        result = list()
+        valid = True
+
+        for info in prerequisites:
+            edges[info[1]].append(info[0])
+
+        def dfs(u: int):
+            nonlocal valid
+            visited[u] = 1
+            for v in edges[u]:
+                if visited[v] == 0:
+                    dfs(v)
+                    if not valid:
+                        return
+                elif visited[v] == 1:
+                    valid = False
+                    return
+            visited[u] = 2
+            result.append(u)
+
+        for i in range(numCourses):
+            if valid and not visited[i]:
+                dfs(i)
+
+        return valid
+
+
+class SolutionBFS:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        edges = collections.defaultdict(list)
+        indeg = [0] * numCourses  # in-degree
+
+        for info in prerequisites:
+            edges[info[1]].append(info[0])
+            indeg[info[0]] += 1
+
+        q = collections.deque(u for u in range(numCourses) if indeg[u] == 0)
+        visited = 0
+
+        while q:
+            visited += 1
+            u = q.popleft()
+            for v in edges[u]:
+                indeg[v] -= 1
+                if indeg[v] == 0:
+                    q.append(v)
+
+        return visited == numCourses
