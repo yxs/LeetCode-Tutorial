5005abfa0d729378c813e5805a8cf85470b671e4
yxs <cedarxsye@outlook.com>
2020-08-11 21:24:19 +0800

    One question per day, August 11

---
+++ problems/130.surrounded-regions/Solution.py
@@ -0,0 +1,36 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        if not board:
+            return
+
+        n, m = len(board), len(board[0])  # rows, columns
+
+        def dfs(x, y):
+            if not 0 <= x < n or not 0 <= y < m or board[x][y] != "O":
+                return
+
+                board[x][y] = "#"
+                dfs(x + 1, y)
+                dfs(x - 1, y)
+                dfs(x, y + 1)
+                dfs(x, y - 1)
+
+        # 左右
+        for i in range(n):
+            dfs(i, 0)
+            dfs(i, m - 1)
+        # 上下
+        for i in range(m - 1):
+            dfs(0, i)
+            dfs(n - 1, i)
+
+        for i in range(n):
+            for j in range(m):
+                if board[i][j] == "#":
+                    board[i][j] = "O"
+                elif board[i][j] == "O":
+                    board[i][j] = "X"
+
